bb74ebb08c1205feead66e00e85ee1dee61cbd37
loganlapierre <lapierr3@uwm.edu>
2024-06-22 16:30:40 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import ProductList from './components/ProductList';
+import Contact from './components/Contact';
+import './styles.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<ProductList />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <footer className="footer">
+          © 2024 Mock Shopping Site. All rights reserved.
+        </footer>
+      </div>
+    </Router>
   );
 }
 
